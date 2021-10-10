e6bd417c835c92b3b6681965ae826b8fa270880c
Ali Bahaari <alibahaari1997@gmail.com>
2021-10-10 14:04:58 +0330

    Random Identifier - Not Completed

--- src/components/Mail.js
+++ src/components/Mail.js
@@ -2,6 +2,10 @@ import '../styles/Mail.css';
 import { useRef } from 'react';
 import { decrementBadge } from '../redux/slices/NavbarSlice';
 import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+
+const clicked = {}
+const random = uuidv4();
 
 function Mail() {
 
@@ -11,13 +15,14 @@ function Mail() {
     const clickMail = () => {
 
         mailRef.current.style.backgroundColor = '#00A9A5';
+        console.log(mailRef.current.dataset.random);
 
         dispatch(decrementBadge());
 
     }
 
     return (
-        <div className="mail" onClick={clickMail} ref={mailRef}>
+        <div className="mail" onClick={clickMail} data-random={random} ref={mailRef}>
             <p>Click on Me and Then See the Badge!</p>
         </div>
     )
